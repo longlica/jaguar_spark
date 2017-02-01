8a2ac1c63c05ac9a60980d9b4822a5f6336b46a1
captain listree <li@listree.com>
2017-02-01 12:34:37 -0800

    insert million records

---
+++ src/main/scala/TestInsertMill.scala
@@ -0,0 +1,48 @@
+import java.sql.DriverManager
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection._
+
+/**
+	* Jaguar SQL:
+	* 	create table jbench (key: uid char(16), addr char(32))
+	*/
+
+object TestInsertMill {
+
+	def main(args: Array[String]) {
+			sparkfunc("NODE_", 10000000)
+	}
+
+	def sparkfunc(prefix: String, max: Int)
+	{
+		Class.forName("com.jaguar.jdbc.JaguarDriver");
+		val sparkConf = new SparkConf().setAppName("TestScalaJDBC")
+		val sc = new SparkContext(sparkConf)
+		val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+		import sqlContext.implicits._
+
+		val url = "jdbc:jaguar://127.0.0.1:8888/test";
+
+		val conn= DriverManager.getConnection(url, "admin", "jaguar" );
+		val st = conn.prepareStatement("insert into jbench values (prepare_0000, prepare_0000)")
+		st.executeUpdate()
+
+		val st2 = conn.createStatement()
+		var n = 1
+		while( n < max ) {
+			val sqlstr = "insert into jbench values (" + prefix + n + "," + prefix + n + ")"
+			st2.executeUpdate(sqlstr)
+			n = n + 1
+		}
+
+		// works
+		val df = sqlContext.sql("SELECT count(*) FROM jbench")
+		df.show()
+
+	}
+
+}
+
