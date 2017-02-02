10c36b45042a7d7f0b66fafe4999d7a0aea561ff
captain listree <li@listree.com>
2017-02-01 17:44:59 -0800

    use a smaller table to test data frame

--- src/main/scala/TestDataFrame.scala
+++ src/main/scala/TestDataFrame.scala
@@ -5,6 +5,10 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection._
 
+/**
+	* Jaguar SQL:
+	* 	create table jdataframe (key: uid char(16), addr char(32))
+	*/
 
 object TestDataFrame {
 
@@ -15,85 +19,55 @@ object TestDataFrame {
 	def sparkfunc()
 	{
 		Class.forName("com.jaguar.jdbc.JaguarDriver");
-		val sparkConf = new SparkConf().setAppName("TestScalaJDBC")
+		val sparkConf = new SparkConf().setAppName("TestDataFrame")
 		val sc = new SparkContext(sparkConf)
 		val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 		import sqlContext.implicits._
 
 		val url = "jdbc:jaguar://127.0.0.1:8888/test";
-		val jbench = sqlContext.read.format("jdbc")
+		val jdataframe = sqlContext.read.format("jdbc")
 		   .options(
 		       Map( "url" -> url,
-						 "dbtable" -> "int10k",
+						 "dbtable" -> "jdataframe",
 						 "user" -> "admin",
 						 "password" -> "jaguar",
-						 "partitionColumn" -> "k1",
-						 "lowerBound" -> "2",
-						 "upperBound" -> "2000000",
-						 "numPartitions" -> "4",
+//						 "partitionColumn" -> "k1",
+//						 "lowerBound" -> "2",
+//						 "upperBound" -> "2000000",
+//						 "numPartitions" -> "4",
 						 "driver" -> "com.jaguar.jdbc.JaguarDriver"
 		   )).load()
 		    
 		// works fine
-		jbench.registerTempTable("jbench")
-		jbench.printSchema()
+		jdataframe.registerTempTable("jbench")
+		jdataframe.printSchema()
 
-		jbench.sort("k2").show()
-		jbench.sort($"k2".desc).show()
-		jbench.sort($"k2".desc, $"k1".asc).show()
-		jbench.orderBy($"k2".desc, $"k1".asc).show()
-		jbench.toJSON.coalesce(1).saveAsTextFile( "/tmp/jbench.txt" );
-
-		// works fine
-		// people.selectExpr("k2", "k1"  ).show()
-		// people.selectExpr("k2", "k1 as keyone"  ).show()
-		//jbench.selectExpr("k2", "k1 as keyone", "abs(k2)" ).show()
-
-		// not work
-		// people.insertInto( "int10k_2", false );
-		// people.write().jdbc(url, "int10k_2", true );
-		//jbench.insertIntoJDBC(url, "jbench_2", true );
-
+		jdataframe.sort("k2").show()
+		jdataframe.sort($"k2".desc).show()
+		jdataframe.sort($"k2".desc, $"k1".asc).show()
+		jdataframe.orderBy($"k2".desc, $"k1".asc).show()
+		jdataframe.toJSON.coalesce(1).saveAsTextFile( "/tmp/jbench.txt" );
 
 		// works fine
-//		val k12 = jbench.select("k1", "k2")
-//		k12.show();
-//
-//		// works fine
-//		val sk1 = jbench.select("k1")
-//		sk1.show();
-//
-//		// works fine
-//		val sk2 = jbench.select("k2")
-//		sk2.show();
-//
-//		// works fine
-//		val below60 = jbench.filter(jbench("k1") <= 3927570 ).show()
+		jdataframe.groupBy("addr").count().show()
 
 		// works fine
-		jbench.groupBy("addr").count().show()
-
-		// works fine
-		jbench.groupBy("addr").avg().show()
-
-
-//		// works great
-//		jbench.describe( "k1", "k2").show()
+		jdataframe.groupBy("addr").avg().show()
 
 		// works great 
-		jbench.stat.freqItems( Seq("k1") ).show()
+		jdataframe.stat.freqItems( Seq("addr") ).show()
 
-		jbench.sample( true, 0.1, 100 ).show()
-		jbench.distinct.show()
-		jbench.dropDuplicates().show()
-		jbench.dropDuplicates.show()
+		jdataframe.sample( true, 0.1, 100 ).show()
+		jdataframe.distinct.show()
+		jdataframe.dropDuplicates().show()
+		jdataframe.dropDuplicates.show()
 
 		// works fine
-		jbench.dropDuplicates.cache.show()
-		jbench.dropDuplicates.persist.show()
+		jdataframe.dropDuplicates.cache.show()
+		jdataframe.dropDuplicates.persist.show()
 
 		// works
-		val df = sqlContext.sql("SELECT * FROM jbench where addr between 'NODE6' and 'NODE7' ")
+		val df = sqlContext.sql("SELECT * FROM jdataframe where addr between 'NODE6' and 'NODE7' ")
 		df.distinct.show()
 
 	}
